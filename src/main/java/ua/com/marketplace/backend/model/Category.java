9dfa2ea53fe2b722622ccdecf7bf1d89dc5bf8fd
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-11-27 22:13:17 +0200

    Refactored product category model   Added field for category image   Added category name field

--- src/main/java/ua/com/marketplace/backend/model/Category.java
+++ src/main/java/ua/com/marketplace/backend/model/Category.java
@@ -2,6 +2,8 @@ package ua.com.marketplace.backend.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
@@ -11,7 +13,13 @@ import java.util.Set;
 @Document(collection = "categories")
 public class Category {
 
+    @Id
     private String id;
+
+    @Indexed(unique = true)
+    private String categoryName;
+
+    private String categoryImage;
 
     private String parent;
 
