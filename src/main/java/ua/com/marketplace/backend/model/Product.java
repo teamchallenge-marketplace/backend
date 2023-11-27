29a76416cbce9260e28ab100c42ad032a4a834f1
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-11-27 22:29:58 +0200

    Refactored product model   Removed redundant compound index creation

--- src/main/java/ua/com/marketplace/backend/model/Product.java
+++ src/main/java/ua/com/marketplace/backend/model/Product.java
@@ -2,20 +2,16 @@ package ua.com.marketplace.backend.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Set;
 
 @Data
 @Builder
 @Document(collection = "products")
-@CompoundIndex(name = "product_code", def = "{sellerId: 1, mpn: 1, gtin: 1}", unique = true)
 public class Product {
 
     @Id
@@ -25,9 +21,14 @@ public class Product {
 
     private String description;
 
-    private String mpn; //Manufacturer part number - product code assigned by manufacturer(seller)
+    /*
+        Manufacturer part number - product code assigned by manufacturer(seller)
+        MPN should remain the same for products with different size, color and other not valuable props
+        !!! red T-Shirt and green T-Shirt should have the same mpn value !!!
+     */
+    private String mpn;
 
-    private String gtin; // International product code GTIN-13
+    private String gtin; // International product code GTIN-13 for books ISBN-13
 
     private Set<String> images;
 
